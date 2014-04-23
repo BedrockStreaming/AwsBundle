9d33f70d2836a6eeef9790be03ddf5ee34013df6
Ugo GLINEL <ugo.glinel@m6web.fr>
2014-04-23 17:34:55 +0200

    Init Aws Bundle

---
+++ src/M6Web/Bundle/AwsBundle/Aws/ClientFactory.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace M6Web\Bundle\AwsBundle\Aws;
+
+/**
+ * ClientFactory
+ */
+class ClientFactory
+{
+    /**
+     * @var string
+     */
+    const AWS_FACTORY_CLASS = 'Aws\Common\Aws';
+
+    /**
+     * @var array
+     */
+    private $aliasKeys;
+
+    /**
+     * __construct
+     *
+     * @param string $factoryClass Factory class name
+     * @param array $aliasKeys Alias key list
+     */
+    public function __construct($factoryClass,array $aliasKeys = array())
+    {
+        if (!$this->testFactoryClass($factoryClass)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'factoryClass extend or be "%s"',
+                    self::AWS_FACTORY_CLASS
+                )
+            );
+        }
+
+        $this->factoryClass = $factoryClass;
+        $this->aliasKeys    = $aliasKeys;
+    }
+
+    /**
+     * testFactoryClass
+     *
+     * @param string $factoryClass Factory class name
+     *
+     * @return boolean
+     */
+    private function testFactoryClass($factoryClass)
+    {
+        if ($factoryClass == self::AWS_FACTORY_CLASS) {
+            return true;
+        }
+
+        $reflection = new \ReflectionClass($factoryClass);
+
+        if ($reflection->isSubclassOf(self::AWS_FACTORY_CLASS)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * filterAliasKey
+     *
+     * @param string $name Config name
+     *
+     * @return string
+     */
+    protected function filterAliasKey($name)
+    {
+        if (in_array($name, $this->aliasKeys)) {
+            return str_replace('_', '.', $name);
+        }
+
+        return $name;
+    }
+
+    /**
+     * get
+     *
+     * @param string $service Aws Service alias
+     * @param array $config   Base config client
+     *
+     * @return AwsClient
+     */
+    public function get($service, array $config)
+    {
+        $params = array();
+
+        foreach ($config as $name => $value) {
+            $params[$this->filterAliasKey($name)] = $value;
+        }
+
+        $factory = $this->factoryClass;
+        $aws     = $factory::factory($params);
+
+        return $aws->get($service);
+    }
+
+    /**
+     * setAliasKeys
+     *
+     * @param array $aliasKeys Alias key list
+     *
+     * @return ClientFactory
+     */
+    public function setAliasKeys(array $aliasKeys)
+    {
+        $this->aliasKeys = $aliasKeys;
+
+        return $this;
+    }
+
+    /**
+     * getAliasKeys
+     *
+     * @return array
+     */
+    public function getAliasKeys()
+    {
+        return $this->aliasKeys;
+    }
+
+}
---
+++ src/M6Web/Bundle/AwsBundle/M6WebAwsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace M6Web\Bundle\AwsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class M6WebAwsBundle extends Bundle
+{
+}
