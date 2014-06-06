fb362733b25c2f26d94aacbf5124e56cb84134db
Sylvain GUYON <sguyon.externe@m6.fr>
2014-06-06 16:25:06 +0200

    Creation of the STS Client

---
+++ src/M6Web/Bundle/AwsBundle/Aws/Sts/Client.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace M6Web\Bundle\AwsBundle\Aws\Sts;
+
+use Aws\Sts\StsClient;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Aws\Common\Credentials\Credentials;
+use Guzzle\Service\Resource\Model;
+
+/**
+ * Sts Client
+ */
+class Client
+{
+    /**
+     * @var StsClient
+     */
+    protected $client;
+
+    /**
+     * @var EventDispatcher
+     */
+    protected $eventDispatcher;
+
+    /**
+     * __construct
+     *
+     * @param DynamoDbClient $client Aws Sts Client
+     */
+    public function __construct(StsClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEventDispatcher(EventDispatcher $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+
+        return $this;
+    }
+
+    /**
+     * Direct access to the Sts client
+     *
+     * @return StsClient
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Creates a credentials object from the credential data return by an STS operation.
+     * 
+     * @param Model $result The result of an STS operation
+     * 
+     * @return Credentials
+     *
+     * @throws Aws\Common\Exception\InvalidArgumentException If the result does not contain credential data
+     */
+    public function createCredentials(Model $result)
+    {
+        return $this->client->createCredentials($result);
+    }
+
+
+    /**
+     * Returns a set of temporary security credentials 
+     * (consisting of an access key ID, a secret access key, and a security token) 
+     * that you can use to access AWS resources that you might not normally have access to. 
+     * Typically, you use AssumeRole for cross-account access or federation.
+     * 
+     * @param string  $roleArn         The Amazon Resource Name (ARN) of the role that the caller is assuming.
+     * @param string  $roleSessionName An identifier for the assumed role session. The session name is included as part of the AssumedRoleUser.
+     * @param string  $policy          An IAM policy in JSON format.
+     * @param integer $durationSeconds The duration, in seconds, of the role session. The value can range from 900 seconds (15 minutes) to 3600 seconds (1 hour).
+     * @param string  $externalId      A unique identifier that is used by third parties to assume a role in their customers' accounts.
+     * @param string  $serialNumber    The identification number of the MFA device that is associated with the user who is making the AssumeRole call.
+     * @param string  $tokenCode       The value provided by the MFA device, if the trust policy of the role being assumed requires MFA.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_assumeRole
+     */
+    public function assumeRole(
+        $roleArn,
+        $roleSessionName,
+        $policy = null,
+        $durationSeconds = 3600,
+        $externalId = null,
+        $serialNumber = null,
+        $tokenCode = null
+    )
+    {
+        $args = [
+            'RoleArn'         => $roleArn,
+            'RoleSessionName' => $roleSessionName,
+            'DurationSeconds' => $durationSeconds
+        ];
+
+        if ($policy !== null) {
+            $args['Policy'] = $policy;
+        }
+
+        if ($externalId !== null) {
+            $args['ExternalId'] = $externalId;
+        }
+
+        if ($serialNumber !== null) {
+            $args['SerialNumber'] = $serialNumber;
+        }
+
+        if ($tokenCode !== null) {
+            $args['TokenCode'] = $tokenCode;
+        }
+
+        return $this->client->assumeRole($args);
+    }
+
+    /**
+     * Returns a set of temporary security credentials for users who have been 
+     * authenticated via a SAML authentication response. 
+     * This operation provides a mechanism for tying an enterprise identity 
+     * store or directory to role-based AWS access without user-specific 
+     * credentials or configuration.
+     * 
+     * @param string  $roleArn         The Amazon Resource Name (ARN) of the role that the caller is assuming.
+     * @param string  $principalArn    The Amazon Resource Name (ARN) of the SAML provider in IAM that describes the IdP.
+     * @param string  $SAMLAssertion   The base-64 encoded SAML authentication response provided by the IdP.
+     * @param string  $policy          An IAM policy in JSON format.
+     * @param integer $durationSeconds The duration, in seconds, of the role session.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_assumeRoleWithSAML
+     */
+    public function assumeRoleWithSAML($roleArn, $principalArn, $SAMLAssertion, $policy = null, $durationSeconds = 3600)
+    {
+        $args = [
+            'RoleArn'         => $roleArn,
+            'PrincipalArn'    => $principalArn,
+            'SAMLAssertion'   => $SAMLAssertion,
+            'DurationSeconds' => $durationSeconds
+        ];
+
+        if ($policy !== null) {
+            $args['Policy'] = $policy;
+        }
+
+        return $this->client->assumeRoleWithSAML($args);
+    }
+
+    /**
+     * Returns a set of temporary security credentials for users who have been authenticated in a mobile 
+     * or web application with a web identity provider, such as Login with Amazon, Facebook, or Google.
+     *
+     * @param string  $roleArn          The Amazon Resource Name (ARN) of the role that the caller is assuming.
+     * @param string  $roleSessionName  An identifier for the assumed role session.
+     * @param string  $webIdentityToken The OAuth 2.0 access token or OpenID Connect ID token that is provided by the identity provider.
+     * @param string  $providerId       The fully-qualified host component of the domain name of the identity provider.
+     * @param string  $policy           An IAM policy in JSON format.
+     * @param integer $durationSeconds  The duration, in seconds, of the role session.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_assumeRoleWithWebIdentity
+     */
+    public function assumeRoleWithWebIdentity(
+        $roleArn,
+        $roleSessionName,
+        $webIdentityToken,
+        $providerId = null,
+        $policy = null,
+        $durationSeconds = 3600
+    )
+    {
+        $args = [
+            'RoleArn'          => $roleArn,
+            'RoleSessionName'  => $roleSessionName,
+            'WebIdentityToken' => $webIdentityToken,
+            'DurationSeconds'  => $durationSeconds
+        ];
+
+        if ($providerId !== null) {
+            $args['ProviderId'] = $providerId;
+        }
+
+        if ($policy !== null) {
+            $args['Policy'] = $policy;
+        }
+
+        return $this->client->assumeRoleWithWebIdentity($args);
+    }
+
+    /**
+     * Decodes additional information about the authorization status of a request
+     * from an encoded message returned in response to an AWS request.
+     * 
+     * @param string $encodedMessage The encoded message that was returned with the response.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_decodeAuthorizationMessage
+     */
+    public function decodeAuthorizationMessage($encodedMessage)
+    {
+        return $this->client->decodeAuthorizationMessage(['EncodedMessage' => $encodedMessage]);
+    }
+
+    /**
+     * Returns a set of temporary security credentials (consisting of an access key ID, a secret access key, 
+     * and a security token) for a federated user.
+     * 
+     * @param string  $name            The name of the federated user.
+     * @param string  $policy          An IAM policy in JSON format that is passed with the GetFederationToken call and evaluated along with the policy or policies that are attached to the IAM user whose credentials are used to call GetFederationToken.
+     * @param integer $durationSeconds The duration, in seconds, that the session should last.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_getFederationToken
+     */
+    public function getFederationToken($name, $policy = null, $durationSeconds = 43200)
+    {
+        $args = [
+            'Name'            => $name,
+            'DurationSeconds' => $durationSeconds
+        ];
+
+        if ($policy !== null) {
+            $args['Policy'] = $policy;
+        }
+
+        return $this->client->getFederationToken($args);
+    }
+
+    /**
+     * Returns a set of temporary credentials for an AWS account or IAM user.
+     * 
+     * @param integer $durationSeconds The duration, in seconds, that the credentials should remain valid.
+     * @param string  $serialNumber    The identification number of the MFA device that is associated with the IAM user who is making the GetSessionToken call.
+     * @param string  $tokenCode       The value provided by the MFA device, if MFA is required.
+     * 
+     * @return Guzzle\Service\Resource\Model
+     *
+     * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.Sts.StsClient.html#_getSessionToken
+     */
+    public function getSessionToken($durationSeconds = 43200, $serialNumber = null, $tokenCode = null)
+    {
+        $args = [
+            'DurationSeconds' => $durationSeconds
+        ];
+
+        if ($serialNumber !== null) {
+            $args['SerialNumber'] = $serialNumber;
+        }
+
+        if ($tokenCode !== null) {
+            $args['TokenCode'] = $tokenCode;
+        }
+
+        return $this->client->getSessionToken($args);
+    }
+}
---
+++ src/M6Web/Bundle/AwsBundle/Aws/Sts/Proxy.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace M6Web\Bundle\AwsBundle\Aws\Sts;
+
+use Aws\Sts\Exception\StsException;
+
+/**
+ * Sts Client Proxy
+ */
+class Proxy
+{
+    /**
+     * Sts Client
+     * 
+     * @var Sts\Client
+     */
+    protected $client;
+
+    /**
+     * Event dispatcher
+     * 
+     * @var Object
+     */
+    protected $eventDispatcher = null;
+
+    /**
+     * Class of the event notifier
+     * 
+     * @var string
+     */
+    protected $eventClass = null;
+
+    /**
+     * __construct
+     * 
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Direct access to the Sts Client
+     * 
+     * @return Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Notify an event to the event dispatcher
+     *
+     * @param string $command   The command name
+     * @param array  $arguments Args of the command
+     * @param int    $time      Exec time
+     *
+     * @return void
+     */
+    public function notifyEvent($command, $arguments, $time = 0)
+    {
+        if ($this->eventDispatcher) {
+            $className = $this->eventClass;
+
+            $event = new $className();
+            $event->setCommand($command);
+            $event->setExecutionTime($time);
+            $event->setArguments($arguments);
+
+            $this->eventDispatcher->dispatch('sts.command', $event);
+        }
+    }
+
+    /**
+     * Set an event dispatcher to notify redis command
+     *
+     * @param Object $eventDispatcher The eventDispatcher object, which implement the notify method
+     * @param string $eventClass The event class used to create an event and send it to the event dispatcher
+     *
+     * @return void
+     */
+    public function setEventDispatcher($eventDispatcher, $eventClass)
+    {
+        if (!is_object($eventDispatcher) || !method_exists($eventDispatcher, 'dispatch')) {
+            throw new Exception("The EventDispatcher must be an object and implement a dispatch method");
+        }
+
+        $class = new \ReflectionClass($eventClass);
+        if (!$class->implementsInterface('\M6Web\Bundle\AwsBundle\Event\DispatcherInterface')) {
+            throw new Exception("The Event class : ".$eventClass." must implement DispatcherInterface");
+        }
+
+        $this->eventDispatcher = $eventDispatcher;
+        $this->eventClass      = $eventClass;
+    }
+
+    /**
+     * Magic access to the Sts client
+     *
+     * @param string $name      Method name
+     * @param array  $arguments Method arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        if ($client = $this->getClient()) {
+            $start = microtime(true);
+            try {
+                $ret = call_user_func_array(array($client, $name), $arguments);
+                $this->notifyEvent($name, $arguments, microtime(true) - $start);
+
+                return $ret;
+            } catch (StsException  $e) {
+                throw new Exception("Error calling the method " . $name . " : " . $e->getMessage());
+            }
+        } else {
+            throw new Exception("Cant connect to Sts");
+        }
+    }
+}
